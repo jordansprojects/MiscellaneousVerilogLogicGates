8ff87b5bf9049b61fe6f6430e0ad9bbcb5eee534
jordansprojects <jordsvpn@protonmail.com>
2022-10-27 12:26:28 -0700

    ALU test bench complete

--- ALU.v
+++ ALU.v
@@ -1,10 +1,10 @@
 module ALU (ALUctl, A, B, ALUOut, Zero);
-input [3:0] ALUctl;
-input [31:0] A,B;
-output reg [31:0] ALUOut;
-output Zero;
-assign Zero = (ALUOut == 0); //Zero is true if ALUOut is 0
-always @(ALUctl, A,B) //reevaluate if these change
+	input [3:0] ALUctl;
+	input [31:0] A,B;
+	output reg [31:0] ALUOut;
+	output Zero;
+	assign Zero = (ALUOut == 0); //Zero is true if ALUOut is 0
+	always @(ALUctl, A,B) //reevaluate if these change
 	case (ALUctl)
 		0: ALUOut <= A & B;
 		1: ALUOut <= A | B;
